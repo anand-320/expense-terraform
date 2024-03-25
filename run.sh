e44b6525880419b7f7c3d3baa57753bc4b9eb9bc
anand <anandanand99599@gmail.com>
2024-03-25 21:53:27 +0530

    simple output

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 env=$1
 action=$2
 
-if [ -z "$env"]; then
+if [ -z "$env" ]; then
   echo "input (dev|qa|state|prod) is not provided"
   exit 1
 fi
 
-if [ -z "$action"]; then
+if [ -z "$action" ]; then
   echo "input action (apply|destroy) is not provided"
-  exit 2
+  exit 1
 fi
 
 rm -rf .terraform/terraform.tfstate
