ac783fc710fedff3403b9a8bc3e1452da6460103
anand <anandanand99599@gmail.com>
2024-03-28 22:18:47 +0530

    terraform expense project

--- run.sh
+++ run.sh
@@ -11,6 +11,7 @@ if [ -z "$action" ]; then
   exit 1
 fi
 
+
 rm -rf .terraform/terraform.tfstate
 terraform init -backend-config=env-$env/state.tfvars
 terraform $action -var-file=env-$env/main.tfvars -auto-approve
