95397e1de22496296df5f11990de9e74bfdaa173
anand <anandanand99599@gmail.com>
2024-03-28 19:42:01 +0530

    terraform expense project

--- provisioners/main.tf
+++ provisioners/main.tf
@@ -22,17 +22,25 @@ resource "aws_instance" "test" {
 data "aws_security_group" "selected" {
   name = "allow-all"
 }
-resource "null_resource" "test1" {
+resource "aws_instance" "test1" {
+  ami                    = "ami-05f020f5935e52dc4"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
+}
+
+resource "null_resource" "provisioner" {
   provisioner "remote-exec" {
-    ami           = "ami-05f020f5935e52dc4"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ data.aws_security_group.selected.id ]
 
+    connection {
+      type     = "ssh"
+      user     = "ec2-user"
+      password = "DevOps321"
+      host     = aws_instance.test1.public_ip
+    }
 
-  }
-
-  inline = {
-    "sudo dnf install nginx -y",
-    "sudo systemctl start nginx"
+    inline = [
+      "dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
   }
 }
