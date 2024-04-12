ba08f840bc7ff1dfa05bd387a4b08c1ad852d984
anand <anandanand99599@gmail.com>
2024-04-12 20:04:59 +0530

    adding vault token to terraform

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "vault-internal.janand.online"
+  address         = "vault-internal.janand.online:8200"
   skip_tls_verify = true
   token           = var.vault_token
 }
