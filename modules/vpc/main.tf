68191f28bd0fe9325333c837ccfa97ff6c94dc19
anand <anandanand99599@gmail.com>
2024-04-21 15:41:09 +0530

    adding nat gateway and routes

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,6 +25,59 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count        = length(var.public_subnet)
+  vpc_id       = aws_vpc.main.id
+  cidr_block   = var.public_subnet[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.env}-public-subnet-${count.index+1}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  count          = length(var.public_subnet)
+  vpc_id         = aws_vpc.main.id
+
+  route {
+    cidr_block = var.default_vpc_cidr
+    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  route {
+    cidr_block = 0.0.0.0/0
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-public-rt-${count.index+1}"
+  }
+}
+
+resource "aws_eip" "ngw" {
+  count   = length(var.public_subnet)
+  domain  = "vpc"
+
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count          = length(var.public_subnet)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.env}-ngw-${count.index+1}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
+
+
 resource "aws_subnet" "frontend" {
   count        = length(var.frontend_subnet)
   vpc_id       = aws_vpc.main.id
@@ -43,6 +96,11 @@ resource "aws_route_table" "frontend" {
   route {
     cidr_block = var.default_vpc_cidr
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+  }
+
+  route {
+    cidr_block = 0.0.0.0/0
+    nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
   tags = {
@@ -76,6 +134,11 @@ resource "aws_route_table" "backend" {
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
+  route {
+    cidr_block = 0.0.0.0/0
+    nat_gateway_id = aws_nat_gateway.ngw[count.index].id
+  }
+
   tags = {
     Name = "${var.env}-backend-rt-${count.index+1}"
   }
@@ -107,6 +170,11 @@ resource "aws_route_table" "db" {
     vpc_peering_connection_id = aws_vpc_peering_connection.main.id
   }
 
+  route {
+    cidr_block = 0.0.0.0/0
+    nat_gateway_id = aws_nat_gateway.ngw[count.index].id
+  }
+
   tags = {
     Name = "${var.env}-db-rt-${count.index+1}"
   }
@@ -118,36 +186,6 @@ resource "aws_route_table_association" "db" {
   route_table_id = aws_route_table.db[count.index].id
 }
 
-resource "aws_subnet" "public" {
-  count        = length(var.public_subnet)
-  vpc_id       = aws_vpc.main.id
-  cidr_block   = var.public_subnet[count.index]
-  availability_zone = var.availability_zones[count.index]
-
-  tags = {
-    Name = "${var.env}-public-subnet-${count.index+1}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  count          = length(var.public_subnet)
-  vpc_id         = aws_vpc.main.id
-
-  route {
-    cidr_block = var.default_vpc_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-  }
-
-  tags = {
-    Name = "${var.env}-public-rt-${count.index+1}"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = length(var.public_subnet)
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public[count.index].id
-}
 
 
 resource "aws_route" "default-vpc" {
