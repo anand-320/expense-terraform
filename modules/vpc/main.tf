3ff967b3abedfbfe94f240c51cb75b835ef7afb7
anand <anandanand99599@gmail.com>
2024-04-21 15:44:19 +0530

    adding nat gateway and routes.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table" "public" {
   }
 
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 
@@ -99,7 +99,7 @@ resource "aws_route_table" "frontend" {
   }
 
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
@@ -135,7 +135,7 @@ resource "aws_route_table" "backend" {
   }
 
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
@@ -171,7 +171,7 @@ resource "aws_route_table" "db" {
   }
 
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw[count.index].id
   }
 
