1c78cbdb08b028f0260b12c00b135e9e5100d87e
anand <anandanand99599@gmail.com>
2024-04-22 20:27:03 +0530

    updating dns record.

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -106,11 +106,12 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  count    = var.lb_needed ? 1 : 0
-  name     = "${var.env}-${var.component}-tg"
-  port     = var.app_port
-  protocol = "HTTP"
-  vpc_id   = var.vpc_id
+  count                = var.lb_needed ? 1 : 0
+  name                 = "${var.env}-${var.component}-tg"
+  port                 = var.app_port
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 15
 
   health_check {
     healthy_threshold   = 2
@@ -119,7 +120,7 @@ resource "aws_lb_target_group" "main" {
     port                = var.app_port
     timeout             = 2
     unhealthy_threshold = 2
-    deregistration_delay = 15
+
   }
 }
 
