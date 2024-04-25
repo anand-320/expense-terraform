6015277e7e397d92e2157c7ad4e66107a9fa16e1
anand <anandanand99599@gmail.com>
2024-04-25 21:20:02 +0530

    aetting asg

---
+++ modules/app-asg/userdata.sh
@@ -0,0 +1,5 @@
+# bin/bash
+
+sudo pip3.11 install ansible hvac &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/anand-320/expense-ansible.git get-secrets.yml -e env=${env} -e role_name=${component} -e vault_token=${var.vault_token} &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/anand-320/expense-ansible.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
