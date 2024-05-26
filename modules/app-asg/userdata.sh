13764f95c71372c7c78de81b5bc2172f33bbfe5d
anand <anandanand99599@gmail.com>
2024-05-26 11:39:35 +0530

    skipping dependencies install during Deployment

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -2,4 +2,4 @@
 
 sudo pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/anand-320/expense-ansible.git get-secrets.yml -e env=${env} -e role_name=${component} -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/anand-320/expense-ansible.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/anand-320/expense-ansible.git expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json -e only_deployment=false &>>/opt/ansible.log
