9cd5c2f999accb0d1c9b17e1a1b381999ea663bf
anand <anandanand99599@gmail.com>
2024-08-12 20:06:29 +0530

    eks cluster

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -9,16 +9,12 @@ resource "aws_eks_cluster" "cluster" {
 
 resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.cluster.name
-  node_group_name = "${var.env}-eks-ng-1"
+  node_group_name = "${var.env}-eks-ng"
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_ids
   capacity_type   = "SPOT"
   instance_types  = ["t3.large"]
 
-  launch_template {
-    name    = "eks-${var.env}"
-    version = "$Latest"
-  }
 
   scaling_config {
     desired_size = 1
